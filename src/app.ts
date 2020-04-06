39d44b77cbc7808a951cfbc2280712bb9ee443fd
aguaholic <joyterra@gmail.com>
2020-04-06 16:52:50 +0200

    Fetch an address form Google geocoding API

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,34 @@
-// Code goes here!
+import axios from 'axios'
+import { GOOGLE_API_KEY } from './constants'
+
+const form = document.querySelector('form')!
+const addressInput = document.getElementById('address')! as HTMLInputElement
+
+type GoogleGeocodingResponse = {
+    results: { geometry: { location:{ lat: number, lng: number } } }[];
+    status: 'OK' | 'ZERO_RESULTS'
+    }
+
+
+function searchAddressHandler(event: Event) {
+    event.preventDefault()
+    const enteredAddress = addressInput.value
+
+    axios.get<GoogleGeocodingResponse>(`https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(
+        enteredAddress
+        )}&key=${GOOGLE_API_KEY}`)
+        .then(response => {
+            if (response.data.status !== 'OK') {
+                throw new Error('Could not fetch location')
+            }
+            const coordinates = response.data.results[0].geometry.location
+            console.log(response)
+        })
+        .catch(err => {
+            alert(err.message)
+            console.log(err)
+        })
+}
+
+
+form.addEventListener('submit', searchAddressHandler )
